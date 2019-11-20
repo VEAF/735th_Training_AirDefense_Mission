4b39147c2652f6a9c197e079b9dcdc1b0ad9a3bb
David Pierron <dpierron@gmail.com>
2019-11-20 11:37:58 +0100

    modified the build scripts
    this change was needed if we want to be able to have multiple users building this mission
    Please copy setup_EXAMPLE.cmd to a new file and edit it locally to make it match your configuration ; then add it to the .gitignore file so it's not source controlled.

--- build.cmd
+++ build.cmd
@@ -1,31 +1,38 @@
-set SEVENZIP=C:\Program Files\7-Zip\7z.exe
-rem - if set to true then the mission will not log anything lower than INFO
-set DEBUG_FLAG=false
-set MISSION_FILE_SUFFIX=%date:~-4,4%%date:~-7,2%%date:~-10,2%
-
-
-rem @echo off
+@echo off
 echo -
 echo ----------------------------------------
 echo building Training Air Defense Mission
 echo ----------------------------------------
 
 rem -- prepare the folders
-rd /s /q .\build
-mkdir .\build
-mkdir .\build\tempsrc
-
-
-@echo off
-echo -
-echo ------------------------------
-echo building caucasus
-echo ------------------------------
+rd /s /q .\build  
+mkdir .\build  
+mkdir .\build\tempsrc  
 
 set VERSION=caucasus
 
+rem -- default options values
+IF [%DEBUG_FLAG%] == [] GOTO DefineDefaultDEBUG_FLAG
+goto DontDefineDefaultDEBUG_FLAG
+:DefineDefaultDEBUG_FLAG
+set DEBUG_FLAG=true
+:DontDefineDefaultDEBUG_FLAG
+echo DEBUG_FLAG = %DEBUG_FLAG%
+
+IF [%MISSION_FILE_SUFFIX%] == [] GOTO DefineDefaultMISSION_FILE_SUFFIX
+goto DontDefineDefaultMISSION_FILE_SUFFIX
+:DefineDefaultMISSION_FILE_SUFFIX
+set MISSION_FILE_SUFFIX=%date:~-4,4%%date:~-7,2%%date:~-10,2%
+:DontDefineDefaultMISSION_FILE_SUFFIX
 set MISSION_FILE=.\build\TR_AD_%VERSION%_%MISSION_FILE_SUFFIX%
 echo MISSION_FILE = %MISSION_FILE%.miz
+
+IF ["%SEVENZIP%"] == [] GOTO DefineDefaultSEVENZIP
+goto DontDefineDefaultSEVENZIP
+:DefineDefaultSEVENZIP
+set SEVENZIP=7za
+:DontDefineDefaultSEVENZIP
+echo SEVENZIP = %SEVENZIP%
 
 rem -- copy all the source mission files
 xcopy /y /e src\%VERSION% .\build\tempsrc\ >nul 2>&1
@@ -49,6 +56,7 @@ copy .\src\scripts\veaf\veafMarkers.lua .\build\tempsrc\l10n\Default  >nul 2>&1
 copy .\src\scripts\veaf\veafMove.lua .\build\tempsrc\l10n\Default  >nul 2>&1
 copy .\src\scripts\veaf\veafNamedPoints.lua .\build\tempsrc\l10n\Default  >nul 2>&1
 copy .\src\scripts\veaf\veafRadio.lua .\build\tempsrc\l10n\Default  >nul 2>&1
+copy .\src\scripts\veaf\veafSecurity.lua .\build\tempsrc\l10n\Default  >nul 2>&1
 copy .\src\scripts\veaf\veafSpawn.lua .\build\tempsrc\l10n\Default  >nul 2>&1
 copy .\src\scripts\veaf\veafTransportMission.lua .\build\tempsrc\l10n\Default  >nul 2>&1
 copy .\src\scripts\veaf\veafUnits.lua .\build\tempsrc\l10n\Default  >nul 2>&1
@@ -59,12 +67,13 @@ copy .\src\scripts\mission-specific\%VERSION%\*.lua .\build\tempsrc\l10n\Default
 
 rem -- set the debug flag to off
 powershell -Command "(gc .\build\tempsrc\l10n\Default\veaf.lua) -replace 'veaf.Development = %DEBUG_FLAG%', 'veaf.Development = false' | sc .\build\tempsrc\l10n\Default\veaf.lua" >nul 2>&1
+powershell -Command "(gc .\build\tempsrc\l10n\Default\veaf.lua) -replace 'veaf.SecurityDisabled = %SECURITY_FLAG%', 'veaf.SecurityDisabled = false' | sc .\build\tempsrc\l10n\Default\veaf.lua" >nul 2>&1
 
 rem -- compile the mission
 "%SEVENZIP%" a -r -tzip %MISSION_FILE%.miz .\build\tempsrc\* -mem=AES256 >nul 2>&1
 
-rem -- delete temporary folder
-rd /s /q .\build\tempsrc
+rem -- cleanup
+rd /s /q .\build\tempsrc  
 
 rem -- done !
 echo Built %MISSION_FILE%.miz
