0079df84a42fa6693472bcda39a6c27353f116d9
David Pierron <dpierron@gmail.com>
2019-11-22 17:37:46 +0100

    updated to reflect changes to the VEAF scripts repository
    (see https://github.com/VEAF/VEAF-Mission-Creation-Tools)

--- extract.cmd
+++ extract.cmd
@@ -1,11 +1,19 @@
 @echo off
+set MISSION_NAME=TR_AD
 echo -
-echo ------------------------------
-echo extracting Caucasus
-echo ------------------------------
+echo ----------------------------------------
+echo extracting Training Air Defense Mission
+echo ----------------------------------------
 
+rem -- default options values
+IF [%SEVENZIP%] == [] GOTO DefineDefaultSEVENZIP
+goto DontDefineDefaultSEVENZIP
+:DefineDefaultSEVENZIP
+set SEVENZIP=7za
+:DontDefineDefaultSEVENZIP
+echo SEVENZIP = %SEVENZIP%
 rem extracting MIZ files
-pushd src\caucasus
+pushd src\mission
 "%SEVENZIP%" x -y ../../*.miz
 del /f /q l10n\Default\*.lua
 popd
