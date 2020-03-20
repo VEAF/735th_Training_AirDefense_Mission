6995051444995336253f5fefae59ad976d96d0f7
David Pierron <dpierron@gmail.com>
2020-03-20 16:56:03 +0100

    Multiple changes
    - updated to v1.4.1 of the Mission Creation Tools
    - added the Interpreter and the Shortcuts module
    - changed to build system to integrate the weather and time versions
    - updated the folders to match (see previous point)
    - set the presets injector to the new frequency plan

--- build.cmd
+++ build.cmd
@@ -91,8 +91,11 @@ xcopy /y /e src\scripts\mission-specific\*.lua .\build\tempsrc\l10n\Default\  >n
 rem -- set the radio presets according to the settings file
 echo set the radio presets according to the settings file
 pushd node_modules\veaf-mission-creation-tools\scripts\veaf
-"%LUA%" veafMissionRadioPresetsEditor.lua  ..\..\..\..\build\tempsrc ..\..\..\..\src\radioSettings.lua %LUA_SCRIPTS_DEBUG_PARAMETER%
+"%LUA%" veafMissionRadioPresetsEditor.lua  ..\..\..\..\build\tempsrc ..\..\..\..\src\radio\radioSettings.lua %LUA_SCRIPTS_DEBUG_PARAMETER%
 popd
+
+rem -- copy the documentation images to the kneeboard
+xcopy /y /e doc\*.jpg .\build\tempsrc\KNEEBOARD\IMAGES\ >nul 2>&1
 
 rem -- copy all the community scripts
 copy .\src\scripts\community\*.lua .\build\tempsrc\l10n\Default  >nul 2>&1
@@ -105,8 +108,8 @@ rem -- set the flags in the scripts according to the options
 powershell -Command "(gc .\build\tempsrc\l10n\Default\veaf.lua) -replace 'veaf.Development = false', 'veaf.Development = %VERBOSE_LOG_FLAG%' | sc .\build\tempsrc\l10n\Default\veaf.lua" >nul 2>&1
 powershell -Command "(gc .\build\tempsrc\l10n\Default\veaf.lua) -replace 'veaf.SecurityDisabled = false', 'veaf.SecurityDisabled = %SECURITY_DISABLED_FLAG%' | sc .\build\tempsrc\l10n\Default\veaf.lua" >nul 2>&1
 
-rem -- compile the mission
-"%SEVENZIP%" a -r -tzip %MISSION_FILE%.miz .\build\tempsrc\* -mem=AES256 >nul 2>&1
+rem -- normalize and prepare the weather and time version 
+FOR %%f IN (.\src\weatherAndTime\*.lua) DO call normalize.cmd %%~nf
 
 rem -- cleanup
 rd /s /q .\build\tempsrc
---
+++ src/weatherAndTime/morning-clear.lua
@@ -0,0 +1,81 @@
+weatherAndTime = {
+    ["date"] = {
+        ["Day"] = 23,
+        ["Month"] = 6,
+        ["Year"] = 2020,
+    }, -- end of ["date"]
+
+    ["start_time"] = 28800,
+
+    ["weather"] = {
+        ["atmosphere_type"] = 0,
+        ["clouds"] = {
+            ["base"] = 2100,
+            ["density"] = 3,
+            ["iprecptns"] = 0,
+            ["thickness"] = 1600,
+        }, -- end of ["clouds"]
+        ["cyclones"] = {
+            [1] = {
+                ["centerX"] = 300000,
+                ["centerZ"] = -900000,
+                ["ellipticity"] = 1,
+                ["pressure_excess"] = -1311,
+                ["pressure_spread"] = 895563.55565682,
+                ["rotation"] = -0.2538764493966,
+            }, -- end of [1]
+            [2] = {
+                ["centerX"] = -1000000,
+                ["centerZ"] = 1800000,
+                ["ellipticity"] = 1,
+                ["pressure_excess"] = -1704,
+                ["pressure_spread"] = 879834.34389462,
+                ["rotation"] = -0.2538764493966,
+            }, -- end of [2]
+            [3] = {
+                ["centerX"] = 900000,
+                ["centerZ"] = -2100000,
+                ["ellipticity"] = 1,
+                ["pressure_excess"] = -1704,
+                ["pressure_spread"] = 879834.34389462,
+                ["rotation"] = -0.2538764493966,
+            }, -- end of [3]
+        }, -- end of ["cyclones"]
+        ["dust_density"] = 0,
+        ["enable_dust"] = false,
+        ["enable_fog"] = false,
+        ["fog"] = {
+            ["thickness"] = 0,
+            ["visibility"] = 0,
+        }, -- end of ["fog"]
+        ["groundTurbulence"] = 0,
+        ["name"] = "VEAF - realistic 3 cyclones and fog in Winter",
+        ["name_cn"] = "夏季 雨天",
+        ["name_de"] = "Sommer, Regen",
+        ["name_es"] = "Verano. Lluvia",
+        ["name_fr"] = "Eté, averses",
+        ["name_ru"] = "Лето. Дождь",
+        ["qnh"] = 760,
+        ["season"] = {
+            ["temperature"] = 20,
+        }, -- end of ["season"]
+        ["type_weather"] = 0,
+        ["visibility"] = {
+            ["distance"] = 80000,
+        }, -- end of ["visibility"]
+        ["wind"] = {
+            ["at2000"] = {
+                ["dir"] = 250,
+                ["speed"] = 10.308,
+            }, -- end of ["at2000"]
+            ["at8000"] = {
+                ["dir"] = 210,
+                ["speed"] = 25.77,
+            }, -- end of ["at8000"]
+            ["atGround"] = {
+                ["dir"] = 270,
+                ["speed"] = 7.731,
+            }, -- end of ["atGround"]
+        }, -- end of ["wind"]
+    }, -- end of ["weather"]
+}
